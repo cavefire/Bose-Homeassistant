7e127de55c56db31c840b851c21ef4c5f617a6f1
Olen <github@olen.net>
2025-12-18 12:12:29 +0100

    feat: improve token refresh (#66)

--- custom_components/bose/const.py
+++ custom_components/bose/const.py
@@ -4,4 +4,10 @@ import logging
 
 DOMAIN = "bose"
 
+# Token Refresh Delay is how long between each check if the token is valid
+# Token Retry Delay is how long before each retry if refresh fails
+TOKEN_REFRESH_DELAY = 3600 # seconds
+TOKEN_RETRY_DELAY = 120 # seconds
+
+
 _LOGGER = logging.getLogger("bose")
